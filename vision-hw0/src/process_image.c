c95f6fbb90919cccafba6af12b482a04bba1d7b2
aaron10l <aaronalvarez7394@gmail.com>
2024-06-09 20:16:07 -0700

    implement get_pixel, set_pixel, copy_image, rgb_to_grayscale

--- vision-hw0/src/process_image.c
+++ vision-hw0/src/process_image.c
@@ -5,29 +5,83 @@
 #include "image.h"
 
 float get_pixel(image im, int x, int y, int c)
-{
-    
-    return 0;
+{ 
+  // checking that x is in bounds, clipping if not
+  if (x >= im.w){
+    x = im.w - 1; 
+  }
+  if (x < 0){
+    x = 0;
+  }
+
+  // checking that y is in bounds, clipping if not
+  if (y >= im.h){
+    y = im.h - 1; 
+  }
+  if (y < 0){
+    y = 0;
+  }
+
+  // checking that channel is in bounds, clipping if not
+  if (c >= im.c){
+    c = im.c - 1; 
+  }
+  
+  if (c < 0){
+    c = 0;
+  }
+
+  // returning desired index
+  int index = x + y*im.w + c*im.w*im.h;
+  return im.data[index];
+  
 }
+
 
 void set_pixel(image im, int x, int y, int c, float v)
 {
-    // TODO Fill this in
+  if ((x >= im.w) || (x < 0)){
+    return;
+  }
+   if ((y >= im.h) || (y < 0)){
+    return;
+  }
+   if ((c >= im.c) || (c < 0)){
+    return;
+  }
+
+  int index = x + y*im.w + c*im.w*im.h;
+  im.data[index] = v;
 }
+
 
 image copy_image(image im)
 {
-    image copy = make_image(im.w, im.h, im.c);
-    // TODO Fill this in
-    return copy;
+  image copy = make_image(im.w, im.h, im.c); 
+  int i = 0;
+
+  for (i = 0; i < im.w * im.h * im.c; i++){
+    copy.data[i] = im.data[i];
+  }
+  return copy;
 }
 
 image rgb_to_grayscale(image im)
 {
-    assert(im.c == 3);
-    image gray = make_image(im.w, im.h, 1);
-    // TODO Fill this in
-    return gray;
+  assert(im.c == 3);
+  image gray = make_image(im.w, im.h, 1);
+
+  float luma_val = 0;
+  int r;
+  int c;
+
+  for (r = 0; r < im.w; r++){
+    for (c = 0; c < im.h; c++ /*  :P  */){
+      luma_val = get_pixel(im, r, c, 0) + get_pixel(im, r, c, 1) + get_pixel(im, r, c, 2);
+      set_pixel(gray, r, c, 0, luma_val);
+    }
+  }
+  return gray;
 }
 
 void shift_image(image im, int c, float v)
